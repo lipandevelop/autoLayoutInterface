265595281b0cecafe8c91ddfaff0dd7de2eeca1b
Li Pan <l.pan.toronto@gmail.com>
2016-01-19 15:14:52 -0500

    Working Version

---
+++ autoLayoutInterface/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  autoLayoutInterface
+//
+//  Created by Li Pan on 2016-01-19.
+//  Copyright © 2016 Li Pan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
